70c68389a42313a62dd221229716f70a86256c43
vdegtjar <v.degtjar@cft.ru>
2023-12-18 16:32:19 +0300

    two commit1

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class LoginTests {
+
+    @Test
+    public void succesfullLoginTest() {
+        Configuration.holdBrowserOpen = true;кккк
+        Configuration.browser = "firefox";
+        open("https://qa.guru/ cms/system/login");
+        $("[name=email]").setValue("bgobq@mailto.plus");
+        $("[name=password]").setValue("$4C$bZxc4");
+        $(".btn-success").click();
+        $(".main-header__login").click();
+        $(".logined-form").shouldHave(text("Здравствуйте, Валиков"));
+    }
+
+    @Test
+    public void unsuccesfullLoginTest() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browser = "firefox";
+        open("https://qa.guru/cms/system/login");
+        $("[name=email]").setValue("bgobq@mailto1.plus");
+        $("[name=password]").setValue("$4C$bZxc4");
+        $(".btn-success").click();
+        $(".login-fo rm").shouldHave(text("Неверный пароль"));
+    }
+
+}
