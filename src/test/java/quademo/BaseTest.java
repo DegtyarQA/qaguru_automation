89f23e8e462ea429addb939ef4304515b3b321dc
vdegtjar <v.degtjar@cft.ru>
2024-01-21 22:56:12 +0300

    add properties gradle4

--- src/test/java/quademo/BaseTest.java
+++ src/test/java/quademo/BaseTest.java
@@ -21,8 +21,8 @@ public class BaseTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.browserSize = "1920x1080";
-        //Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
         //WebDriverRunner.getWebDriver().manage().window().maximize();
         Configuration.browser = "chrome";
         Configuration.browserVersion="100.0";
